7c636e883bdf5b894390aa5411ac8fed13db3fb5
ivoamu-2 <115148258+ivoamu-2@users.noreply.github.com>
2022-10-06 11:27:45 +0200

    Add files via upload

---
+++ algo.py
@@ -0,0 +1,25 @@
+import random as r
+import time
+def ssum(array,a,b):
+        if (a != b):
+            arr = array[a:b]
+            return sum(arr)
+def solve(array):
+    ss = 0
+    for l in range(len(array)):
+        for i in range(len(array)-l+1):
+            s = ssum(array,l,i+l)
+            if s != None:
+                if s > ss:
+                    ss = s
+    return ss
+array = []
+for i in range(10):
+    n = r.randint(-100,100)
+    array.append(n)
+a = time.time()
+print(0)
+ss = solve(array)
+print(ss)
+b = time.time()
+print(b-a)
